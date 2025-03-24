a917c410e678bc3e865e3f54426511811fba42ca
PhamVanPhuc2k2 <phamvanphuctnp@gmail.com>
2025-03-24 14:31:56 +0700

    Create new users

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,3 +1,13 @@
+// Get the client
+import mysql from 'mysql2';
+
+// Create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'abc',
+});
+
 const handleHellWorld = (req, res) => {
     return res.render('home.ejs');
 };
@@ -6,7 +16,25 @@ const handleUserPage = (req, res) => {
     return res.render('user.ejs');
 };
 
+const handleCreateNewUser = (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    let username = req.body.username;
+
+    connection.query(
+        'INSERT INTO users (email, password, username) VALUES (?, ?, ?)',
+        [email, password, username],
+        function (err, results, fields) {
+            console.log(results);
+            console.log(fields);
+        },
+    );
+
+    return res.send('handleCreateNewUser');
+};
+
 export default {
     handleHellWorld,
     handleUserPage,
+    handleCreateNewUser,
 };
